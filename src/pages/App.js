97978c420120f63b8d196d76de9dd611ca047a7b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-06-16 20:20:09 +0700

    การเข้าถึง dom element ด้วย refs
    การเข้าถึง dom element ด้วย refs

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,16 +4,12 @@ import FbButton from '../components/FbButton'
 
 
 class App extends Component {
-    state = {like:0, dislike:0}
-    componentWillMount(){
-        console.log('App: willMoint');
-    }
     componentDidMount(){
-        console.log('App: didMoint')
+        this.txt2.focus();
     }
+    state = {like:0, dislike:0}
     render() {
         let {like, dislike} = this.state
-        console.log('App: render')
         return ( 
             <div>
             <Header title="I am Header" like={like}/>
@@ -21,6 +17,12 @@ class App extends Component {
             <h4>DisLike: {dislike}</h4>
             <FbButton handleClick={this.onLike} caption="Like"/>
             <FbButton handleClick={this.onDisLike} caption="DisLike"/>
+            <input type="text" ref={(input) => {
+                this.txt1 = input;
+            }}/>
+            <input type="text" ref={(input) => {
+                this.txt2 = input;
+            }}/>
             </div>
         )
     }
@@ -50,3 +52,11 @@ class App extends Component {
 
 }
 export default App
+/* 
+การเข้าถึง DOM element ด้วย Ref
+ปกติใน Jquery เวลาที่เราต้องการ set focus ให้กับ textbox เราจะสั่ง $('#textid').focus() สำหรับ Rect นั้น เราต้องใช้ Ref โดยมีวิธีใช้งานแบบนี้
+เราต้องการเข้าถึง DOM ตัวไหนเราก็ระบุ props ว่า ref แล้วกำหนดชื่อให้มัน ซึ้งก็เหมือนใน javascript ทั้วๆไปที่มีการกำหนด id จากนั้นเราจะเข้าถึง ref ได้ที่ componentDidMount() โดยเรียก this.refs.txt2 สังเกตุว่า refs นี้เติม s นะครับแล้วต่อด้วยชื่อของ reft จากนั้นระบุว่าจะทำอะไร โดยส่วนใหญ่เป็นการ set focus ด้วยคำสั่ง focus() ครับ
+การกำหนด ref วิธีข้างบนทาง React เขาไม่แนะนำให้ใช้ แต่เขาให้ใช้อีกวิธีครับเป็นลักษณะของการกำหนดแบบ function เหมือนเดิมครับ 
+
+รูปแบบการประกาศ ref ก็คือ ref={(input) => {this.txt1=input}} โดยที่ input คือตัวแปรที่เราตั้งขึ้นครับเป็นอะไรก็ได้ไม่ต้องเป็นคำว่า input ส่วนการเข้าถึง ref ใน componentDidMount() ก็เปลียนจาก this.refs เป็นอ้างถึง this.txt2 คือชื่อของ ref ได้เลยครับ
+*/
